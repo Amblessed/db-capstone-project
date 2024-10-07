1d3b5a5da03bee708b6f086586c17044dd973879
Okechukwu Bright Onwumere <brighthamza9@yahoo.com>
2024-10-07 02:37:59 +0200

    Added Task as Comments in the script

--- AddValidBooking_Procedure.sql
+++ AddValidBooking_Procedure.sql
@@ -1,3 +1,9 @@
+-- Task 3
+-- For your third and final task, Little Lemon need to verify a booking, and decline any reservations for tables that are already booked under another name. 
+-- Since integrity is not optional, Little Lemon need to ensure that every booking attempt includes these verification and decline steps. However, implementing these steps requires a stored procedure and a transaction. 
+-- To implement these steps, you need to create a new procedure called AddValidBooking. This procedure must use a transaction statement to perform a rollback if a customer reserves a table that’s already booked under another name.  
+
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `AddValidBooking`(IN bookingDate DATE, IN tableNumber INT)
 BEGIN
 START TRANSACTION; 
--- CheckBooking_Procedure.sql
+++ CheckBooking_Procedure.sql
@@ -1,3 +1,7 @@
+-- Task 2
+-- For your second task, Little Lemon need you to create a stored procedure called CheckBooking to check whether a table in the restaurant is already booked. Creating this procedure helps to minimize the effort involved in repeatedly coding the same SQL statements.
+-- The procedure should have two input parameters in the form of booking date and table number. You can also create a variable in the procedure to check the status of each table.
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `CheckBooking`(IN bookingDate DATE, IN tableNumber INT)
 BEGIN
 	IF EXISTS(SELECT * FROM Bookings WHERE BookingDate = bookingDate AND TableNumber = tableNumber) THEN
